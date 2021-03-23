6c022addebee3fa310baea4daf8bd5ee7c0cc00e
Hernan Matias Belizan <hernanbelizan@hotmail.com>
2021-03-23 02:06:07 -0300

    WIP: Ejercicio 12

---
+++ 1C2021/EjercicioGuia1/app12/index.php
@@ -0,0 +1,19 @@
+<?php
+//Hernásn Matias Belizán
+
+
+// Aplicación Nº 12 (Invertir palabra)
+//Realizar el desarrollo de una función que reciba un Array de caracteres y que invierta el orden
+//de las letras del Array.
+//Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”
+include "libreria/funciones.php";
+
+$arrayEntrada = array('H','O','L','A');
+
+$salida = returString($arrayEntrada);
+
+var_dump($salida);
+
+echo $salida;
+
+?>
