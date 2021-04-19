e24d07b82d1847224dd51b0648d1c8e19389f5f9
Hernan Matias Belizan <hernanbelizan@hotmail.com>
2021-04-19 06:26:39 -0300

    Completo ejercicio 8

---
+++ 1C2021/EjercicioGuia1/app08/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Aplicación Nº 8 (Carga aleatoria)
+// Imprima los valores del vector asociativo siguiente usando la estructura de control ​foreach​:
+// $v[1]=90; $v[30]=7; $v['e']=99; $v['hola']= 'mundo';
+
+$v[1]=90; $v[30]=7; $v['e']=99; $v['hola']= 'mundo';
+
+foreach ($v as $key => $value) {
+    
+    if($value == 'mundo'){
+        echo $key . " " . $value;
+        echo "<br/>";
+    }else
+        echo $value;
+        echo "<br/>";
+    
+}
+
+?>
